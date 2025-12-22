1aa12205f924925d3cb6636d14dc2df74abd78a0
Priyanshu <04priyanshuuuu@gmail.com>
2025-12-22 07:54:22 +0530

    added cloudinary

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,9 +55,9 @@ INSTALLED_APPS = [
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": "xxxxx",
-    "API_KEY": "xxxxx",
-    "API_SECRET": "xxxxx",
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
 }
 
 
